585b35c9e89cee7d89c892edfad213bffae0b998
Jiancao164 <60164697+Jiancao164@users.noreply.github.com>
2021-01-12 15:35:04 -0800

    update the solution

--- src/CommandTest.java
+++ src/CommandTest.java
@@ -8,24 +8,24 @@ public class CommandTest {
         Command command = new Command();
 
 
-        assertEquals(command.solution("/", "abc"), "/abc");
-        assertEquals(command.solution("/abc/def", "ghi"), "/abc/def/ghi");
-        assertEquals(command.solution("/abc/def", ".."), "/abc");
-        assertEquals(command.solution("/abc/def", "/abc"), "/abc");
-        assertEquals(command.solution("/abc/def", "/abc/klm"), "/abc/klm");
-        assertEquals(command.solution("/abc/def", "../.."), "/");
-        assertEquals(command.solution("/abc/def", "../../.."), "/");
-        assertEquals(command.solution("/abc/def", "."), "/abc/def");
-        assertEquals(command.solution("/abc/def", "..klm"), "No such file or directory");
-        assertEquals(command.solution("/abc/def", "//////"), "/");
-        assertEquals(command.solution("/abc/def", "......"), "No such file or directory");
-        assertEquals(command.solution("/abc/def", "../gh///../klm/."), "/abc/klm");
+        assertEquals("/abc", command.solution("/", "abc"));
+        assertEquals("/abc/def/ghi", command.solution("/abc/def", "ghi"));
+        assertEquals("/abc", command.solution("/abc/def", ".."));
+        assertEquals("/abc", command.solution("/abc/def", "/abc"));
+        assertEquals("/abc/klm", command.solution("/abc/def", "/abc/klm"));
+        assertEquals("/", command.solution("/abc/def", "../.."));
+        assertEquals("/", command.solution("/abc/def", "../../.."));
+        assertEquals("/abc/def", command.solution("/abc/def", "."));
+        assertEquals("No such file or directory", command.solution("/abc/def", "..klm"));
+        assertEquals("/", command.solution("/abc/def", "//////"));
+        assertEquals("No such file or directory", command.solution("/abc/def", "......"));
+        assertEquals("/abc/klm", command.solution("/abc/def", "../gh///../klm/."));
 
-        assertEquals(command.solution("/a/b/c", "d/"), "/a/b/c/d");
-        assertEquals(command.solution("/l/n/m", "o"), "/l/n/m/o");
-        assertEquals(command.solution("/a/b/c", "/./../a/../b/../"), "/");
-        assertEquals(command.solution("/a/b/c", "../../c/"), "/a/c");
-        assertEquals(command.solution("/a/b/c", "../b/c"), "/a/b/b/c");
+        assertEquals( "/a/b/c/d", command.solution("/a/b/c", "d/"));
+        assertEquals("/l/n/m/o", command.solution("/l/n/m", "o"));
+        assertEquals("/", command.solution("/a/b/c", "/./../a/../b/../"));
+        assertEquals("/a/c", command.solution("/a/b/c", "../../c/"));
+        assertEquals("/a/b/b/c", command.solution("/a/b/c", "../b/c"));
 
 
     }
